c4803e1ed3599b434a47e53b14884c49f4c08181
Tawai-hub <erictash44@gmail.com>
2022-10-24 18:07:45 +0300

    simple js project

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('form').onsubmit = () => {
+      const task = document.querySelector('#task').value;
+      const li = document.createElement('li');
+      li.innerHTML = task;
+      document.querySelector('#tasks').append(li);
+      return false;
+    }
+
+})
